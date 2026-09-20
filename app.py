23079dfde05ece925006d2291e52dfb836a968d5
Arunima-aramana <arunimasukumaran2125@gmail.com>
2026-09-20 21:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -644,4 +644,5 @@ if __name__ == "__main__":
     add_aptitude_questions()
     add_interview_questions()
 
+     port = int(os.environ.get("PORT", 5000))
      app.run(host="0.0.0.0", port=5000, debug=True)
